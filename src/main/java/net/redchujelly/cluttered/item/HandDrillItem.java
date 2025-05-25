a4433b03fa728f76fcb724619f729e7bab4f894a
bs124 <bs124691520@gmail.com>
2025-05-25 16:01:20 -0400

    made it so hand drill can't rotate bed halves anymore (rip) (on behalf of yellowchujelly)

--- src/main/java/net/redchujelly/cluttered/item/HandDrillItem.java
+++ src/main/java/net/redchujelly/cluttered/item/HandDrillItem.java
@@ -11,6 +11,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BedBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -300,7 +301,7 @@ public class HandDrillItem extends Item {
         BlockState nextState = getNextBlock(state);
         if (pContext.getPlayer().isCrouching()){
             if (!level.isClientSide) {
-                if (state.getBlock() instanceof MultiblockPlacer){
+                if (state.getBlock() instanceof MultiblockPlacer || state.getBlock() instanceof BedBlock){
                     return InteractionResult.PASS;
                 }
                 if (state.hasProperty(BlockStateProperties.FACING)) {
