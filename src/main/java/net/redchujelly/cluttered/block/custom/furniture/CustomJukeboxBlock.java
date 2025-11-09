2c4aeee1dfe079c168785b216b62902ced53b81b
unilock <katherines1337@outlook.com>
2025-11-09 14:51:03 -0500

    fix jukebox logic

--- src/main/java/net/redchujelly/cluttered/block/custom/furniture/CustomJukeboxBlock.java
+++ src/main/java/net/redchujelly/cluttered/block/custom/furniture/CustomJukeboxBlock.java
@@ -1,21 +1,14 @@
 package net.redchujelly.cluttered.block.custom.furniture;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.ItemInteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.JukeboxPlayable;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.JukeboxBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
@@ -32,17 +25,6 @@ public class CustomJukeboxBlock extends JukeboxBlock {
     public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
         return SHAPE;
     }
-
-	@Override
-	protected ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
-		if (state.getValue(HAS_RECORD)) {
-			return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
-		} else {
-			ItemStack itemstack = player.getItemInHand(hand);
-			ItemInteractionResult iteminteractionresult = JukeboxPlayable.tryInsertIntoJukebox(level, pos, itemstack, player);
-			return !iteminteractionresult.consumesAction() ? ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION : iteminteractionresult;
-		}
-	}
 
     @Nullable
     @Override
